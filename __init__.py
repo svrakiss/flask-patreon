046e7be20a5dedca1e824a28ed8c70ef12152fad
svrakiss <59843464+svrakiss@users.noreply.github.com>
2022-12-08 11:39:55 -0800

    cache responses from patreon api

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import requests_cache
 class MyApp(Flask):
     def __init__(self):
         Flask.__init__(self,__name__)
 
 app = MyApp()
 app.config.from_envvar('CONFIG')
+# client-side request caching (so caching the patreon api responses)
+requests_cache.install_cache('github_cache',backend=app.config['REQUEST_CACHE_BACKEND'],expire_after=app.config['REQUEST_CACHE_TIMEOUT'])
