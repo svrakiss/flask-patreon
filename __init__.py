391642ad49f8c211ff44b90cdddeab5d8b0770a6
svrakiss <59843464+svrakiss@users.noreply.github.com>
2023-01-09 11:46:10 -0800

    readded cache. support for redis

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+import os
 import requests_cache
+from requests_cache.backends.redis import RedisCache
 class MyApp(Flask):
     def __init__(self):
         Flask.__init__(self,__name__)
@@ -7,4 +9,9 @@ class MyApp(Flask):
 app = MyApp()
 app.config.from_envvar('CONFIG')
 # client-side request caching (so caching the patreon api responses)
-# requests_cache.install_cache('github_cache',backend=app.config['REQUEST_CACHE_BACKEND'],expire_after=app.config['REQUEST_CACHE_TIMEOUT'])
+if app.config['REQUEST_CACHE_BACKEND'] == 'redis':
+    backend = RedisCache(host=os.environ.get('REDIS_HOST'),
+    port=os.environ.get('REDIS_PORT'))
+    requests_cache.install_cache('github_cache',backend=backend,expire_after=app.config['REQUEST_CACHE_TIMEOUT'])
+else:
+    requests_cache.install_cache('github_cache',backend=app.config['REQUEST_CACHE_BACKEND'],expire_after=app.config['REQUEST_CACHE_TIMEOUT'])
