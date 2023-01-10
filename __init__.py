e46f0c1559ef1605f78861ffbf424d180b214f62
svrakiss <59843464+svrakiss@users.noreply.github.com>
2023-01-09 18:48:27 -0800

    re-add  redis

--- __init__.py
+++ __init__.py
@@ -10,9 +10,8 @@ app = MyApp()
 app.config.from_envvar('CONFIG')
 # client-side request caching (so caching the patreon api responses)
 if app.config['REQUEST_CACHE_BACKEND'] == 'redis':
-    # backend = RedisCache(host=os.environ.get('REDIS_HOST'),
-    # port=os.environ.get('REDIS_PORT'))
-    # requests_cache.install_cache('github_cache',backend=backend,expire_after=app.config['REQUEST_CACHE_TIMEOUT'])
-    print('Oh you picked REDIS?! how exciting')
+    backend = RedisCache(host=os.environ.get('REDIS_HOST'),
+    port=os.environ.get('REDIS_PORT'))
+    requests_cache.install_cache('github_cache',backend=backend,expire_after=app.config['REQUEST_CACHE_TIMEOUT'])
 else:
     requests_cache.install_cache('github_cache',backend=app.config['REQUEST_CACHE_BACKEND'],expire_after=app.config['REQUEST_CACHE_TIMEOUT'])
